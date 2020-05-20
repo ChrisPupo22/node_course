754fc5955482a736b8d6f6686968e1ac2fa725d9
ChrisPupo22 <cpupo822@gmail.com>
2020-05-20 16:49:46 -0400

    routing with express

--- express_intro/app.js
+++ express_intro/app.js
@@ -4,16 +4,19 @@ const express = require('express');
 
 const app = express(); 
 
-app.use((req, res, next) => {
-    console.log('in the middleware');
-    next(); //Allows the request to continue to the next middleware in line
-}); 
+app.use('/', (req, res, next) => {
+    console.log('this always runs')
+    next(); 
+});
 
-app.use((req, res, next) => {
+app.use('/add-product',(req, res, next) => {
+    console.log('another middleware');
+    res.send('<h1>Add Product Page</h1>'); 
+});
+
+app.use('/',(req, res, next) => {
     console.log('another middleware');
     res.send('<h1>hello from express</h1>'); 
-})
+});
 
-const server = http.createServer(app); 
-
-server.listen(3000); 
+app.listen(3000); 
