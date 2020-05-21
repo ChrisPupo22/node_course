4e729e01a88338524d2b75376970dec9c059a61b
ChrisPupo22 <cpupo822@gmail.com>
2020-05-21 13:10:55 -0400

    creation of e-commerce app

--- express_intro/app.js
+++ express_intro/app.js
@@ -1,22 +1,24 @@
-const http = require('http');
+const express = require('express');
 
-const express = require('express'); 
+const bodyParser = require('body-parser');
+
+const path = require('path'); 
 
 const app = express(); 
 
-app.use('/', (req, res, next) => {
-    console.log('this always runs')
-    next(); 
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/add-product',(req, res, next) => {
-    console.log('another middleware');
-    res.send('<h1>Add Product Page</h1>'); 
-});
 
-app.use('/',(req, res, next) => {
-    console.log('another middleware');
-    res.send('<h1>hello from express</h1>'); 
-});
+app.use(bodyParser.urlencoded({extended: false})); 
+
+app.use('/admin', adminRoutes); 
+app.use(shopRoutes); 
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, '/', 'views', 'page-not-found.html')); 
+}); 
+
+
 
 app.listen(3000); 
